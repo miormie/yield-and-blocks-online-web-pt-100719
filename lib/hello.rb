16a1cc71a6d07ea01585bd26886b0dbb45ee2ade
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-27 06:40:13 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(array)
+if block_given?
 i = 0 
 
 while i < array.length 
@@ -8,8 +9,10 @@ i = i + 1
 end 
   
 array
+else 
+  puts "Hey! No block was given!"
 end
-
+end 
 # # call your method here!
 # hello_t(["Tim", "Tom", "Jim"]) do |name|
 #   if name.start_with?("T")
